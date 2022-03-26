9bf77c5919f5e6835d3de4c1dec6dcaa793c2557
Travis Simmers <travis.s.simmers@gmail.com>
2022-03-26 01:33:03 -0700

    Updated interactions using hands
    Player can now grab and hold onto objects, granted the object has a XR Grab Interactable script added to it.

--- The Magic VR Bus/Assets/Scripts/HandPresensePhysics.cs
+++ The Magic VR Bus/Assets/Scripts/HandPresensePhysics.cs
@@ -6,25 +6,48 @@ public class HandPresensePhysics : MonoBehaviour
 {
     public Transform target;
     private Rigidbody _rigidBody;
+    private Collider[] _handCollidors;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
+        _handCollidors = GetComponentsInChildren<Collider>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         // Update hand position
-        _rigidBody.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
+        //_rigidBody.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
 
         // Update hand rotation
-        Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
-        rotationDifference.ToAngleAxis(out float angleInDegrees, out Vector3 rotationAxis);
+        //Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+        //rotationDifference.ToAngleAxis(out float angleInDegrees, out Vector3 rotationAxis);
         
-        Vector3 rotationDifferenceInDegrees = angleInDegrees * rotationAxis;
+        //Vector3 rotationDifferenceInDegrees = angleInDegrees * rotationAxis;
         
-        _rigidBody.angularVelocity = (rotationDifferenceInDegrees * Mathf.Deg2Rad / Time.fixedDeltaTime);
+        //_rigidBody.angularVelocity = (rotationDifferenceInDegrees * Mathf.Deg2Rad / Time.fixedDeltaTime);
+    }
+
+    public void EnableHandCollidorDelay(float delay)
+    {
+        Invoke("EnableHandCollidor", delay);
+    }
+
+    public void EnableHandCollidor()
+    {
+        foreach(Collider mesh in _handCollidors)
+        {
+            mesh.enabled = true;
+        }
+    }
+
+    public void DisableHandCollidor()
+    {
+        foreach (Collider mesh in _handCollidors)
+        {
+            mesh.enabled = false;
+        }
     }
 }
