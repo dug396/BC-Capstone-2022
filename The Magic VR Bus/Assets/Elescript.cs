1375065fa7f9cdfab8e4413507b5483c02de9086
Andrew Lawson <Andrew.lawson@bellevuecollege.edu>
2022-04-24 15:20:07 -0700

    Added code for test elevator and wheelchair Camera Action

---
+++ The Magic VR Bus/Assets/Elescript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elescript : MonoBehaviour
+{
+    // Start is called before the first frame update
+public float up = 100f;
+public float range = 3f;
+public GameObject elevator;
+public bool isRising = false;
+public Camera fpsCam;
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+        /*
+    public float health = 1f;
+    public void TakeDamage (float amount){
+
+        health -= amount;
+        if (health <= 0f){
+            Die();
+
+        }
+    }
+
+    void Die(){
+        Destroy(gameObject);
+
+    }
+    */
+}
